20a9c74b372a5f5f970813359b3ac342ab9b7be8
Tbouder <tbouder@protonmail.com>
2017-05-27 11:06:33 +0200

    ReadLine

--- helloWorld.kt
+++ helloWorld.kt
@@ -1,9 +1,17 @@
+//Default Main Function to learn Kotlin
+
 fun main(args: Array<String>)
 {
-	val		numberSecond:Int = 42
-	var		number = 3
-	var		string = "Helloooo"
-	var		stringSecond:String? = "helloo"
+	val		numberSecond:Int
+	var		number:Int
+	var		string:String
+	var		stringSecond:String?
 
+	numberSecond = 42
+	println("Enter a Number: ")
+	number = readLine()!!.toInt()
+	string = "Helloooo"
+	println("Enter your Name: ")
+	stringSecond = readLine()!!.toString()
 	println(string + ' ' + stringSecond + ' ' + number + ' ' + numberSecond)
 }
