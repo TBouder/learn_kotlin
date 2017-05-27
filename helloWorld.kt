5e07fcc2eb104317d97dcf76b3063cdb565129f4
Tbouder <tbouder@protonmail.com>
2017-05-27 10:58:47 +0200

    Var and val

--- helloWorld.kt
+++ helloWorld.kt
@@ -1,4 +1,9 @@
 fun main(args: Array<String>)
 {
-    println("Hello, World!")
+	val		numberSecond:Int = 42
+	var		number = 3
+	var		string = "Helloooo"
+	var		stringSecond:String? = "helloo"
+
+	println(string + ' ' + stringSecond + ' ' + number + ' ' + numberSecond)
 }
