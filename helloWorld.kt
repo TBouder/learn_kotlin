7b34505c0d1b82703b7694227cdc3910c1bd005e
Tbouder <tbouder@protonmail.com>
2017-05-27 12:10:08 +0200

    When, While, for, Ternary

--- helloWorld.kt
+++ helloWorld.kt
@@ -2,16 +2,44 @@
 
 fun main(args: Array<String>)
 {
+	/*
+	**	? at the end -> Can be null
+	*/
+	val		Bool:Boolean
 	val		numberSecond:Int
 	var		number:Int
 	var		string:String
 	var		stringSecond:String?
 
-	numberSecond = 42
+	/*
+	**	!! at the end -> Cannot be null
+	*/
+	Bool = true
 	println("Enter a Number: ")
-	number = readLine()!!.toInt()
+	number = 10
 	string = "Helloooo"
 	println("Enter your Name: ")
 	stringSecond = readLine()!!.toString()
+
+	/*
+	**	Output
+	*/
+	//"TERNAIRE"
+	numberSecond = if (!Bool) 7 else 100
 	println(string + ' ' + stringSecond + ' ' + number + ' ' + numberSecond)
+	when (number)
+	{
+		1			-> println("number === 1")
+		2			-> println("number === 2")
+		3			-> println("number === 3")
+		4			-> println("number === 4")
+		in 5..42	-> println("number >= 5 && <= 42")
+		!in 43..70	-> println("number <= 43 || >= 70")
+		else		-> println("Nop")
+	}
+
+	for (item in 1..10)
+		println("Item: " + item)
+	while (number >= 1)
+		println("Number: " + number--)
 }
